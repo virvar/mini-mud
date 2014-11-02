83cd3b0cf32ae6cb4b465a19e84910df8029f8e3
virvar <revalka@gmail.com>
2014-11-02 23:15:10 +0300

    Implement say, whisper, exit.

--- src/mini_mud/logic/core.clj
+++ src/mini_mud/logic/core.clj
@@ -6,25 +6,39 @@
                 2 {:id 2 :objects '(:forrest) :neighbour-locations {:north 3 :south 1}}
                 3 {:id 3 :objects '() :neighbour-locations {:south 2}}})
 
-(def players (atom #{}))
+(def players-by-name (ref {}))
 
-(def players-map (atom {}))
+(def players-map (ref {}))
 
-(def players-sequence (atom 1))
+(def players-sequence (ref 0))
+
+(defn notify-player
+  [player msg]
+  ((:notifier player) msg))
 
 (defn generate-name []
-  (let [name (str "player" @players-sequence)]
-    (swap! players-sequence inc)
-    name))
+  (dosync
+   (str "player" (alter players-sequence inc))))
 
-(defn add-player []
-  (let [location (locations (:start-location-id game-map))
-        player {:name (generate-name) :location location}]
-    (swap! players conj player)
-    (if (contains? @players-map location)
-      (swap! players-map update-in [location] conj player)
-      (swap! players-map assoc location #{player}))
+(defn add-player [notifier]
+  (let [name (generate-name)
+        location (locations (:start-location-id game-map))
+        player {:name name :location location :notifier notifier}]
+    (dosync
+     (alter players-by-name assoc name player)
+     (if (contains? @players-map location)
+       (alter players-map update-in [location] conj player)
+       (alter players-map assoc location #{player})))
+    (println (str name " added"))
     player))
+
+(defn get-player-by-name
+  [name]
+  (players-by-name name))
+
+(defn get-location-info
+  [player]
+  (str (@players-map (:location player))))
 
 (defn move-player
   [player direction]
@@ -32,15 +46,35 @@
     (let [location (:location player)
           new-location (locations ((:neighbour-locations location) direction))
           new-player (assoc player :location new-location)]
-      (swap! players disj player)
-      (swap! players conj new-player)
-      (swap! players-map update-in [location] disj player)
-      (if (contains? @players-map new-location)
-        (swap! players-map update-in [new-location] conj new-player)
-        (swap! players-map assoc new-location #{new-player}))
+      (dosync
+       (alter players-by-name assoc (:name player) new-player)
+       (alter players-map update-in [location] disj player)
+       (if (contains? @players-map new-location)
+         (alter players-map update-in [new-location] conj new-player)
+         (alter players-map assoc new-location #{new-player})))
+      (notify-player new-player (get-location-info new-player))
       new-player)
+                                        ;@TODO notify affected players
     player))
 
-(defn get-location-info
+(defn say
+  [player msg]
+  (println "say")
+  (let [display-msg (str (:name player) ": " msg)]
+    (doseq [neighbour-player (players-map (:location player))]
+      (notify-player neighbour-player display-msg))))
+
+(defn whisper
+  [player to-player-name msg]
+  (println "whisper")
+  (let [to-player (players-by-name to-player-name)
+        display-msg (str (:name player) " шепчет: " msg)]
+    (notify-player to-player display-msg)))
+
+(defn exit-player
   [player]
-  (str (@players-map (:location player))))
+  (dosync
+   (alter players-by-name dissoc (:name player))
+   (alter players-map update-in [(:location player)] disj player))
+                                        ;@TODO notify affected players
+  (println (str (:name player) " exited")))
--- src/mini_mud/network/servers/socket_server.clj
+++ src/mini_mud/network/servers/socket_server.clj
@@ -1,8 +1,8 @@
 (ns mini-mud.network.servers.socket-server
-  (:require [mini-mud.logic.core :as logic] :reload-all)
+  (:require [clojure.string :as str]
+            [mini-mud.logic.core :as logic] :reload-all)
   (:import [java.net ServerSocket Socket SocketException]
-           [java.io InputStreamReader OutputStreamWriter]
-           [clojure.lang LineNumberingPushbackReader]))
+           [java.io InputStreamReader OutputStreamWriter BufferedReader]))
 
 (defn on-thread [f]
   (doto (Thread. f) (.start)))
@@ -16,26 +16,43 @@
                   (recur)))
     socket))
 
+(defn create-player-notifier
+  [writer]
+  (fn [msg]
+    (binding [*out* writer]
+      (println msg)
+      (flush))))
+
 (defn handle-message
   [player msg]
-  (logic/move-player player (keyword msg)))
+  (cond
+   (re-matches #"сказать пользователю .*" msg)
+   (let [cmd (str/split msg #" " 4)
+         to-player-name (nth cmd 2)
+         player-msg (nth cmd 3)]
+     (logic/whisper player to-player-name player-msg))
+   (re-matches #"сказать .*" msg) (logic/say player (nth (str/split msg #" " 2) 1))
+   (= msg "север") (logic/move-player player :north)
+   (= msg "юг") (logic/move-player player :south)
+   (= msg "запад") (logic/move-player player :west)
+   (= msg "восток") (logic/move-player player :east)
+   (= msg "выход") (logic/exit-player player)
+   :else "Неизвестная команда"))
 
 (defn handle-client
   [ins outs]
   (let [eof (Object.)
-        reader (LineNumberingPushbackReader. (InputStreamReader. ins))
+        reader (BufferedReader. (InputStreamReader. ins))
         writer (OutputStreamWriter. outs)
-        init-player (logic/add-player)]
-    (loop [player init-player
-           msg (read reader false eof)]
-      (when-not (= msg eof)
-        (println msg)
-        (flush)
-        (let [new-player (handle-message player msg)]
-          (binding [*out* writer]
-            (println (logic/get-location-info new-player))
-            (flush))
-          (recur new-player (read reader false eof)))))))
+        player-name (:name (logic/add-player (create-player-notifier writer)))]
+    (binding [*in* reader]
+      (loop [msg (read-line)]
+        (when-not (nil? msg)
+          (flush)
+          (let [player (logic/get-player-by-name player-name)]
+            (handle-message player msg)
+            (recur (read-line)))))
+      (logic/exit-player (logic/get-player-by-name player-name)))))
 
 (defn handle-socket
   [s]

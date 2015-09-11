ace769e7b637a97b2c9f133bf4900fc3aba38c39
virvar <revalka@gmail.com>
2015-09-11 13:16:25 +0300

    Handle disconnections.

--- src/mini_mud/network/servers/non_blocking_socket_server.clj
+++ src/mini_mud/network/servers/non_blocking_socket_server.clj
@@ -2,13 +2,55 @@
   (:import [java.nio ByteBuffer CharBuffer]
            [java.nio.channels SelectionKey Selector ServerSocketChannel
             SocketChannel]
-           [java.nio.charset Charset CharsetDecoder CharsetEncoder]))
+           [java.nio.charset Charset CharsetDecoder CharsetEncoder]
+           [java.io IOException]))
 
 (def client-sequence (atom 0))
 
 (defn- create-client-notifier
   [client encoder]
   (fn [msg] (.write client (.encode encoder (CharBuffer/wrap (str msg "\n"))))))
+
+
+(defn- handle-connection
+  [server selector client-sequence client-handler encoder]
+  (let [client (.accept server)]
+    (.configureBlocking client false)
+    (println "New client")
+    (let [client-key (.register client selector SelectionKey/OP_READ)
+          client-id (swap! client-sequence inc)]
+      (.attach client-key client-id)
+      ((:handle-connection client-handler)
+       client-id
+       (create-client-notifier client encoder)))))
+
+(defn- close-client
+  [client-handler key]
+  (let [client (.channel key)]
+    (do (.cancel key)
+      (.close client)
+      (let [client-id (.attachment key)]
+        ((:handle-disconnection client-handler) client-id))
+      (println "Client exited"))))
+
+(defn- handle-message
+  [key buffer decoder client-handler]
+  (let [client (.channel key)]
+    (try
+      (let [bytes-read (.read client buffer)]
+        (if (= bytes-read -1)
+          (close-client client-handler key)
+          (do
+            (.flip buffer)
+            (let [raw-msg (.toString (.decode decoder buffer))
+                  msg (first (clojure.string/split-lines raw-msg))
+                  client-id (.attachment key)]
+              (.clear buffer)
+              (println "client: " msg)
+              ((:handle-message client-handler) client-id msg)))))
+      (catch IOException ex
+        (println ex)
+        (close-client client-handler key)))))
 
 (defn handle-connections
   [server port client-handler]
@@ -24,35 +66,13 @@
       (.register server selector SelectionKey/OP_ACCEPT)
       (while (.isOpen server)
         (.select selector)
-        (doseq [key (.selectedKeys selector)]
-          (cond (.isAcceptable key)
-                (let [client (.accept server)]
-                  (.configureBlocking client false)
-                  (println "New client")
-                  (let [client-key (.register client selector SelectionKey/OP_READ)
-                        client-id (swap! client-sequence inc)]
-                    (.attach client-key client-id)
-                    ((:handle-connection client-handler)
-                     client-id
-                     (create-client-notifier client encoder))))
-                (.isReadable key)
-                (let [client (.channel key)
-                      bytes-read (.read client buffer)]
-                  (if (= bytes-read -1)
-                    (do (.cancel key)
-                        (.close client)
-                        (let [client-id (.attachment key)]
-                          ((:handle-disconnection client-handler) client-id))
-                        (println "Client exited"))
-                    (do
-                      (println "readable")
-                      (.flip buffer)
-                      (let [raw-msg (.toString (.decode decoder buffer))
-                            msg (first (clojure.string/split-lines raw-msg))
-                            client-id (.attachment key)]
-                        (.clear buffer)
-                        (println msg)
-                        ((:handle-message client-handler) client-id msg)))))))
+        (try
+          (doseq [key (.selectedKeys selector)]
+            (cond (.isAcceptable key)
+                  (handle-connection server selector client-sequence client-handler encoder)
+                  (.isReadable key)
+                  (handle-message key buffer decoder client-handler)))
+          (catch Exception ex (do (println ex))))
         (-> selector
             (.selectedKeys)
             (.clear))
